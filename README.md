d193860ae68bd6f21cedb7efe4bd4dae5144eb4a
michele <michele.vantini-1@studenti.unitn.it>
2016-12-14 22:07:06 +0100

    code review done; readme updated;

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 # SE2_16_170670_P
+### Michele Vantini 170670
 Implementation of the mockup realized about meals delivery system.
 
-Attention: for the purpose of this test the menu is present only for January 2017, so via the UI you can add order only for January 2017
+### Attention
+For the purpose of this test the menu is present only for January 2017, so via the UI you can manage order only for January 2017
+
+## How to test (local)
+1. At first you have to clone this repository using the command `git clone https://github.com/MicheleVantini-1/SE2_16_170670_P.git`
+2. Change directory with `cd SE2_16_170670_P`
+3. Once you cloned this repository you have to install the libraries that are not cloned accorging to what specified in the file .gitignore: `npm install`
+4. And now you can launch the server using the command `node index.js` or `heroku local web` (make sure that you have the port 5000 free)
+5. Now you can access the application at http://127.0.0.1:5000
+
+## How to run test cases
+1. At first you have to install jasmine with `npm install -g jasmine`
+2. Then launch all the test cases with `jasmine`
+
+## Heroku
+You can also test the application directly online at http://peaceful-sands-94334.herokuapp.com/
--- code_review/index.js
+++ code_review/index.js
@@ -16,6 +16,7 @@ app.use(session({
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+
 var port = process.env.PORT || 5000;
 app.set('port',port);
 app.listen(port,function(){
@@ -35,7 +36,8 @@ app.get('/login',function(request,response) {
 		},
 		function(data)
 		{
-			response.writeHead(200,{'Content-Type':'text/html'});
+			var statusCode = authenticated ? 307 : 200; //307 redirected because already logged in
+			response.writeHead(statusCode,{'Content-Type':'text/html'});
 			response.end(data);
 		});
 });
@@ -47,8 +49,13 @@ app.post('/authenticate',function(request,response){
 		&& data.correctAuthentication(username,password) && request.session.auth == null)
 	{
 		request.session.auth = username;
+		response.redirect('/');
 	}
-	response.redirect('/');
+	else
+	{
+		response.status = 403;
+		response.sendFile(path.join(__dirname+'/../pages/forbidden.html'));
+	}
 });
 
 app.get('/logout',function(request,response){
@@ -64,7 +71,8 @@ app.get('/signup',function(request,response){
 		},
 		function(data)
 		{
-			response.writeHead(200,{'Content-Type':'text/html'});
+			var statusCode = authenticated ? 307 : 200; 				//307 redirected because already logged in
+			response.writeHead(statusCode,{'Content-Type':'text/html'});
 			response.end(data);
 		});
 });
@@ -72,7 +80,8 @@ app.get('/signup',function(request,response){
 app.post('/registerUser',function(request,response){
 	if(!request.body)
 	{
-		response.redirect('/');
+		response.status=400;
+		response.sendFile(path.join(__dirname+'/../pages/badrequest.html'));
 	}
 	else
 	{
@@ -86,7 +95,7 @@ app.post('/registerUser',function(request,response){
 		{
 			if(!data.insertUser(username,pwd,name,surname))
 			{
-				response.redirect('/signup');
+				response.redirect(400,'/signup');
 			}
 			else
 			{
@@ -95,16 +104,21 @@ app.post('/registerUser',function(request,response){
 		}
 		else
 		{
-			response.redirect('/signup');
+			response.redirect(400,'/signup');
 		}
 
 	}
 });
 
-app.use('/insertTest',function(request,response){
-	if(!request.body)
+app.post('/insertTest',function(request,response){
+	if(request.session.auth == null)
 	{
-		response.redirect('/');
+		response.redirect(401,'/login');
+	}
+	else if(!request.body)
+	{
+		response.status =400;
+		response.sendFile(path.join(__dirname+'/../pages/badrequest.html'));
 	}
 	else
 	{
@@ -112,12 +126,41 @@ app.use('/insertTest',function(request,response){
 		var test = request.body.testname;
 		var date = request.body.date;
 		var score = request.body.score;
-		var universities = request.body.universities.split(",").map(s => s.trim);
+		var universities = request.body.universities.split(",").map(s => s.trim());
 
 		data.insertTest(test);
+
 		for(var i=0; i< universities.length; i++){
 			data.associateUserToTest(username,test,date,universities[i],score);
 		}
+		response.redirect('/');
+	}
+});
+
+app.post('/tests/getUserTests',function(request,response){
+	if(!!request.session.auth)
+	{
+		var headers = {};
+		headers["Access-Control-Allow-Origin"] = "*"; //for cross enviroment request
+		headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";//methods allowed to response
+		headers["Access-Control-Allow-Credentials"] = false;
+		headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"; //type of headers
+		headers["Content-Type"] = "application/json";//format response
+
+		var tests = data.getUserTests(request.session.auth);
+		var json = {tests:[]};
+		for(var i=0;i<tests.length;i++)
+		{
+			json.tests.push(data.testToJSON(tests[i]));
+		}
+
+		response.writeHead(200, headers);
+		response.end(JSON.stringify(json));
+	}
+	else
+	{
+		response.writeHead(401,{});
+		response.end(JSON.stringify({}));
 	}
 });
 
